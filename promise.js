44af1842f9de71877b5e1ef4f8306d8d1f59a52c
Barnabas <Barnabas-chimaobi>
2019-06-07 12:55:44 +0100

    this is my js file

---
+++ promise.js
@@ -0,0 +1,19 @@
+let name = prompt('please enter your name')
+
+let customer = document.querySelector('.name')
+customer.innerHTML = (`welcome ${name}`)
+
+// loadEventListeners()
+//  function loadEventListeners(){
+
+// customer.addEventListener('submit',addName )
+
+//  }
+
+//  function addName(e){
+//    const input = document.createElement('input');
+//    input.appendChild(document.createTextNode(name.value))
+
+
+//   e.preventDefault()
+// }
